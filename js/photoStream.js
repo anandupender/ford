441453c460b62fcf212e79a74947543e2d41b43d
Anand Upender <anand.upender@gmail.com>
2019-07-09 17:05:14 -0400

    audio working with new clickable modal

--- js/photoStream.js
+++ js/photoStream.js
@@ -1,11 +1,23 @@
+var modal = document.getElementById('modal');
+modal.style.display = 'flex';
+modal.style.alignItems = 'center';
+
 var audios = document.getElementsByClassName("info-card");
 for(var i = 0; i < audios.length; i++){
   	audios.item(i).addEventListener("mouseenter", function( event ) {
 		var audio = this.children[0]
     var playPromise = audio.play();
-    if (playPromise !== null){
-      playPromise.catch(() => { audio.play(); })
-    }
+
+    if (playPromise !== undefined) {
+    playPromise.then(_ => {
+
+      console.log("worked");
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+  }
+
 	}, false);
 	audios.item(i).addEventListener("mouseleave", function( event ) {
 		var audio = this.children[0]
